066cef31d319e7d634bea89a0804d9a10b823c63
Dgame <rswhite4@googlemail.com>
2017-04-30 22:39:53 +0200

    Typo

--- tests/ExpectationsTest.php
+++ tests/ExpectationsTest.php
@@ -5,134 +5,134 @@ use function Dgame\Expectation\expect;
 
 final class ExpectationsTest extends TestCase
 {
-    public function testisInt()
+    public function testIsInt()
     {
         $this->assertEquals(42, expect(42)->isInt()->else(23));
         $this->assertEquals(1337, expect('foo')->isInt()->else(1337));
     }
 
-    public function testisFloat()
+    public function testIsFloat()
     {
         $this->assertEquals(4.2, expect(4.2)->isFloat()->else(2.3));
         $this->assertEquals(13.37, expect('foo')->isFloat()->else(13.37));
     }
 
-    public function testisScalar()
+    public function testIsScalar()
     {
         $this->assertEquals('bar', expect('bar')->isScalar()->else('foo'));
         $this->assertEquals(42, expect(42)->isScalar()->else('bar'));
     }
 
-    public function testisNumeric()
+    public function testIsNumeric()
     {
         $this->assertEquals(42, expect(42)->isNumeric()->else('foo'));
         $this->assertEquals('1337', expect('1337')->isNumeric()->else('bar'));
     }
 
-    public function testisString()
+    public function testIsString()
     {
         $this->assertEquals('foo', expect('foo')->isString()->else('bar'));
         $this->assertEquals('bar', expect(42)->isString()->else('bar'));
     }
 
-    public function testisBool()
+    public function testIsBool()
     {
         $this->assertTrue(expect(true)->isBool()->else(false));
         $this->assertFalse(expect(1)->isBool()->else(false));
     }
 
-    public function testisArray()
+    public function testIsArray()
     {
         $this->assertEquals([1, 2, 3], expect([1, 2, 3])->isArray()->else([]));
         $this->assertEquals([2], expect('abc')->isArray()->else([2]));
         $this->assertEquals([], expect(null)->isArray()->else([]));
     }
 
-    public function testisObject()
+    public function testIsObject()
     {
         $this->assertEquals($this, expect($this)->isObject()->else(null));
         $this->assertEquals(null, expect(null)->isObject()->else(null));
         $this->assertEquals(23, expect(42)->isObject()->else(23));
     }
 
-    public function testisInstanceOf()
+    public function testIsInstanceOf()
     {
         $this->assertEquals($this, expect($this)->isInstanceOf(self::class)->else(null));
         $this->assertEquals(null, expect(new Exception())->isInstanceOf(self::class)->else(null));
         $this->assertEquals(null, expect(null)->isInstanceOf(self::class)->else(null));
     }
 
-    public function testisCallable()
+    public function testIsCallable()
     {
         $this->assertEquals('trim', expect('trim')->isCallable()->else(null));
         $this->assertEquals(null, expect('empty')->isCallable()->else(null));
     }
 
-    public function testisDir()
+    public function testIsDir()
     {
         $this->assertEquals(__DIR__, expect(__DIR__)->isDir()->else('src'));
         $this->assertEquals('src', expect('test')->isDir()->else('src'));
     }
 
-    public function testisFile()
+    public function testIsFile()
     {
         $this->assertEquals(__FILE__, expect(__FILE__)->isFile()->else(null));
         $this->assertEquals(null, expect('src')->isFile()->else(null));
     }
 
-    public function testisEmpty()
+    public function testIsEmpty()
     {
         $this->assertEmpty(expect(null)->isEmpty()->else('abc'));
         $this->assertNull(expect('abc')->isEmpty()->else(null));
     }
 
-    public function testisNotEmpty()
+    public function testIsNotEmpty()
     {
         $this->assertNotEmpty(expect(null)->isNotEmpty()->else('abc'));
         $this->assertNotNull(expect('abc')->isNotEmpty()->else(null));
     }
 
-    public function testisNull()
+    public function testIsNull()
     {
         $this->assertNull(expect(null)->isNull()->else('abc'));
         $this->assertEquals('foobar', expect('abc')->isNull()->else('foobar'));
     }
 
-    public function testisNotNull()
+    public function testIsNotNull()
     {
         $this->assertNotNull(expect(null)->isNotNull()->else('abc'));
         $this->assertNull(expect(null)->isNotNull()->else(null));
     }
 
-    public function testisTrue()
+    public function testIsTrue()
     {
         $this->assertTrue(expect(false)->isTrue()->else(true));
         $this->assertTrue(expect(true)->isTrue()->else(false));
         $this->assertFalse(expect(false)->isTrue()->else(false));
     }
 
-    public function testisFalse()
+    public function testIsFalse()
     {
         $this->assertFalse(expect(false)->isFalse()->else(true));
         $this->assertFalse(expect(true)->isFalse()->else(false));
         $this->assertTrue(expect(true)->isFalse()->else(true));
     }
 
-    public function testisEqual()
+    public function testIsEqual()
     {
         $this->assertEquals('abc', expect('abc')->isEqual('abc')->else('foo'));
         $this->assertEquals('42', expect('42')->isEqual('42')->else(null));
         $this->assertEquals(42, expect('42')->isEqual(42)->else(null));
     }
 
-    public function testisNotEqual()
+    public function testIsNotEqual()
     {
         $this->assertEquals('foo', expect('abc')->isNotEqual('abc')->else('foo'));
         $this->assertEquals(null, expect('42')->isNotEqual('42')->else(null));
         $this->assertEquals(42, expect('42')->isEqual(42)->else(null));
     }
 
-    public function testisIdenticalTo()
+    public function testIsIdenticalTo()
     {
         $this->assertEquals('abc', expect('abc')->isIdenticalTo('abc')->else('foo'));
         $this->assertEquals('42', expect('42')->isIdenticalTo('42')->else(null));
@@ -140,7 +140,7 @@ final class ExpectationsTest extends TestCase
         $this->assertEquals(42, expect(42)->isIdenticalTo(42)->else(null));
     }
 
-    public function testisNotIdenticalTo()
+    public function testIsNotIdenticalTo()
     {
         $this->assertEquals('foo', expect('abc')->isNotIdenticalTo('abc')->else('foo'));
         $this->assertEquals(null, expect('42')->isNotIdenticalTo('42')->else(null));
@@ -148,7 +148,7 @@ final class ExpectationsTest extends TestCase
         $this->assertEquals(null, expect(42)->isNotIdenticalTo(42)->else(null));
     }
 
-    public function testisBetween()
+    public function testIsBetween()
     {
         $this->assertEquals(7, expect(7)->isBetween(0, 8)->else(null));
         $this->assertEquals(7, expect(7)->isBetween(0, 7)->else(null));
@@ -165,46 +165,46 @@ final class ExpectationsTest extends TestCase
         $this->assertEquals([42, 23], expect([])->hasLength(2)->else([42, 23]));
     }
 
-    public function testisIn()
+    public function testIsIn()
     {
         $this->assertEquals(42, expect(42)->isIn([1, 2, 23, 36, 42, 44, 48])->else(null));
         $this->assertEquals(42, expect(42)->isIn([1, 2, 23, 36, 42])->else(null));
         $this->assertEquals(null, expect(42)->isIn([1, 2, 23, 36])->else(null));
     }
 
-    public function testisKeyOf()
+    public function testIsKeyOf()
     {
         $this->assertEquals('foo', expect('foo')->isKeyOf(['a' => 23, 'foo' => 42])->else(null));
         $this->assertEquals(null, expect('foo')->isKeyOf(['a' => 23])->else(null));
     }
 
-    public function testisBelow()
+    public function testIsBelow()
     {
         $this->assertEquals(3, expect(3)->isBelow(4)->else(42));
         $this->assertEquals(42, expect(6)->isBelow(4)->else(42));
     }
 
-    public function testisAbove()
+    public function testIsAbove()
     {
         $this->assertEquals(42, expect(3)->isAbove(4)->else(42));
         $this->assertEquals(6, expect(6)->isAbove(4)->else(42));
     }
 
-    public function testisBelowOrEqual()
+    public function testIsBelowOrEqual()
     {
         $this->assertEquals(23, expect(23)->isBelowOrEqual(23)->else(null));
         $this->assertEquals(23, expect(23)->isBelowOrEqual(42)->else(null));
         $this->assertEquals(null, expect(23)->isBelowOrEqual(22)->else(null));
     }
 
-    public function testisAboveOrEqual()
+    public function testIsAboveOrEqual()
     {
         $this->assertEquals(42, expect(42)->isAboveOrEqual(42)->else(null));
         $this->assertEquals(42, expect(42)->isAboveOrEqual(23)->else(null));
         $this->assertEquals(null, expect(42)->isAboveOrEqual(256)->else(null));
     }
 
-    public function testisPositive()
+    public function testIsPositive()
     {
         $this->assertEquals(42, expect(-1)->isPositive()->else(42));
         $this->assertEquals(0, expect(0)->isPositive()->else(42));
